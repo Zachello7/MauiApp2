d9bacf08e36a4967fef26457fc03b99048302993
LeonardoZachelloEnci <LeonardoZachelloEnci@WIN-LN7QD1ES0R8>
2025-10-08 07:44:40 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp2/Components/Model/cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2.Components.Model
+{
+    public class Cliente
+    {
+        public string nome { get; set; } = string.Empty;
+        public string cpf { get; set; } = string.Empty;
+        public string telefone { get; set; } = string.Empty;
+        
+        public static Cliente UltimoCliente {  get; set; } 
+    }
+}
