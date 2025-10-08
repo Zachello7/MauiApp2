a86d0de0c667f3da4450ac74ad3a4e74dbe8dcf1
Zachello7 <00001121337466sp@aluno.educacao.sp.gov.br>
2025-10-08 08:36:36 -0300

    ...

---
+++ MauiApp2/Components/DAO/ClienteDAO.cs
@@ -0,0 +1,29 @@
+﻿using MauiApp2.Components.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace MauiApp2.Components.DAO
+{
+    public class ClienteDAO
+    {
+        public async Task<bool > SalvarCliente(Cliente novoCliente)
+        {
+            try
+            {
+                //string de conexao
+                string connectionString = "server=localhost;user=root;password=root;database=db_empresa_1";
+
+                await using var conn = new MySqlConnection(connectionString);
+            }
+
+            catch
+            {
+
+            }
+        }
+    }
+}
