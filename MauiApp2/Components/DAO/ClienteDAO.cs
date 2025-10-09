a8f41cf22a1ab39c24862dfe60ab0f9537e1c344
Zachello7 <00001121337466sp@aluno.educacao.sp.gov.br>
2025-10-09 09:10:08 -0300

    ...

--- MauiApp2/Components/DAO/ClienteDAO.cs
+++ MauiApp2/Components/DAO/ClienteDAO.cs
@@ -18,11 +18,25 @@ namespace MauiApp2.Components.DAO
                 string connectionString = "server=localhost;user=root;password=root;database=db_empresa_1";
 
                 await using var conn = new MySqlConnection(connectionString);
+
+                await conn.OpenAsync();
+
+                string sql = "INSERT INTO cliente (nome, cpf, telefone) VALUES(@nome, @cpf, @telefone)";
+
+                await using var cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@nome", novoCliente.nome);
+                cmd.Parameters.AddWithValue("@cpf", novoCliente.cpf);
+                cmd.Parameters.AddWithValue("@telefone", novoCliente.telefone);
+
+                int rows = await cmd.ExecuteNonQueryAsync();
+
+                return rows > 0;
+
             }
 
-            catch
+            catch (Exception ex)
             {
-
+                return false;
             }
         }
     }
